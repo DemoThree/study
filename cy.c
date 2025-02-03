339c4150624dce7ec3ce8223c72f48a0005e7a25
DemoThree <89907405+DemoThree@users.noreply.github.com>
2025-02-04 01:58:10 +0800

    2-3winrun

--- cy.c
+++ cy.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 void reverse(int);
-void main(){
-int i;
-scanf("%d",&i);
-if (i<0)
+void main()
 {
-    i=-i;
-    printf("-");
+    int i;
+
+    scanf("%d", &i);
+    if (i < 0)
+    {
+        i = -i;
+        printf("-");
+    }
+
+    reverse(i);
 }
-
-reverse(i);
-
-
-}
-void reverse(int a){
- 
- 
- printf ("%d",a%10);
-a=a/10;
-if (a!=0)
+void reverse(int a)
 {
-    /* code */
-    reverse(a);
-}
 
+    printf("%d", a % 10);
+    a = a / 10;
+    if (a != 0)
+    {
+        /* code */
+        reverse(a);
+    }
 }
---
+++ list/2-3arraylist.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 100
+typedef int ElemType;
+typedef struct sqlist
+{
+    ElemType data[MAX_SIZE];
+    int length;
+} Sqlist;
+int main(int argc, char const *argv[])
+{
+
+        /* code */
+    return 0;
+}

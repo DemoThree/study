1e96814c1cc9aa596ea2be9ae98606d0ddfbea24
DemoThree <89907405+DemoThree@users.noreply.github.com>
2025-02-06 13:05:07 +0800

    ignore .exe

---
+++ list/2-6staticList.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 100
+typedef int ElemType;
+typedef struct sqlist
+{
+    ElemType data[MAX_SIZE];
+    int length;
+
+} sqlist;
+
+int main(int argc, char const *argv[])
+{
+    printf("你好！");
+    return 0;
+}
